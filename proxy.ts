16fde2f7ccff81d6762cbe9740a40a73d6b2fc84
Kim <prakhargupta.regular@gmail.com>
2026-02-20 16:39:23 -0500

    chore: migrate middleware to proxy for Next.js

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { auth } from "./auth";
 
-export const middleware = auth;
+export const proxy = auth;
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
